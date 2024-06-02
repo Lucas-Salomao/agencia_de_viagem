589956e6d6a2f2b898717b52ef9c193bb619ca84
Lucas-Salomao <lucastadeusalomao@gmail.com>
2024-06-02 19:00:57 -0300

    first commit

---
+++ src/main/java/com/example/agencia_de_viagem/service/UsersService.java
@@ -0,0 +1,84 @@
+package com.example.agencia_de_viagem.service;
+
+import com.example.agencia_de_viagem.domain.dto.UsersDTO;
+import com.example.agencia_de_viagem.domain.entity.UserEntity;
+import com.example.agencia_de_viagem.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UsersService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public UsersDTO createUser(UsersDTO userDTO) {
+        UserEntity userEntity = convertToEntity(userDTO);
+        userEntity = userRepository.save(userEntity);
+        return convertToDTO(userEntity);
+    }
+
+    /**
+     * Retorna todos os usuarios cadastrados.
+     *
+     * @return Uma lista com todos os usuarios.
+     */
+    public List<UsersDTO> getAllUsers() {
+        List<UserEntity> usersEntities = userRepository.findAll();
+        return usersEntities.stream()
+                .map(this::convertToDTO) // Utiliza a função toDTO para converter a lista de entidades para DTOs
+                .collect(Collectors.toList());
+    }
+
+    public UsersDTO updateUser(Long id, UsersDTO userDTO) {
+        UserEntity userEntity = userRepository.getReferenceById(id);
+        if(userEntity == null) {
+            return null;
+        }
+
+        userEntity.setUserName(userDTO.getUserName());
+        userEntity.setPassword(userDTO.getPassword());
+        userEntity.setFirstName(userDTO.getFirstName());
+        userEntity.setLastName(userDTO.getLastName());
+        userEntity.setEmail(userDTO.getEmail());
+        userEntity = userRepository.save(userEntity);
+        return convertToDTO(userEntity);
+    }
+
+    public void deleteUserById(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public UsersDTO getUserByUserName(String userName) {
+        UserEntity userEntity = userRepository.findByUserName(userName);
+        if (userEntity != null) {
+            return convertToDTO(userEntity);
+        }
+        return null;
+    }
+
+    private UserEntity convertToEntity(UsersDTO userDTO) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(userDTO.getId());
+        userEntity.setUserName(userDTO.getUserName());
+        userEntity.setPassword(userDTO.getPassword()); // Senha não criptografada
+        userEntity.setFirstName(userDTO.getFirstName());
+        userEntity.setLastName(userDTO.getLastName());
+        userEntity.setEmail(userDTO.getEmail());
+        return userEntity;
+    }
+
+    private UsersDTO convertToDTO(UserEntity userEntity) {
+        UsersDTO userDTO = new UsersDTO();
+        userDTO.setId(userEntity.getId());
+        userDTO.setUserName(userEntity.getUserName());
+        userDTO.setPassword(userEntity.getPassword()); // Senha não criptografada
+        userDTO.setFirstName(userEntity.getFirstName());
+        userDTO.setLastName(userEntity.getLastName());
+        userDTO.setEmail(userEntity.getEmail());
+        return userDTO;
+    }
+}
