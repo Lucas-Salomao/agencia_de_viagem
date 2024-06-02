4602420cb24606127cbeff005653cc8e9654aba6
Lucas-Salomao <lucastadeusalomao@gmail.com>
2024-06-02 19:46:08 -0300

    add new column

--- src/main/resources/db.migrations/V2__cirar_tabela_users.sql
+++ src/main/resources/db.migrations/V2__cirar_tabela_users.sql
@@ -1,9 +1,10 @@
 CREATE SCHEMA users_details;
 CREATE TABLE users_details.users (
-                                     id BIGSERIAL PRIMARY KEY,
-                                     firstname VARCHAR(255) NOT NULL,
-                                     lastname VARCHAR(255) NOT NULL,
-                                     username VARCHAR(255) NOT NULL,
-                                     password VARCHAR(255) NOT NULL,
-                                     email VARCHAR(255) NOT NULL
+                                    id BIGSERIAL PRIMARY KEY,
+                                    firstname VARCHAR(255) NOT NULL,
+                                    lastname VARCHAR(255) NOT NULL,
+                                    username VARCHAR(255) NOT NULL,
+                                    password VARCHAR(255) NOT NULL,
+                                    email VARCHAR(255) NOT NULL,
+                                    role VARCHAR(255) NOT NULL
 );
